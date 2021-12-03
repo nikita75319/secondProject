8977a791c56c0bbe910660effd102dc0c22e1e81
nikita75319 <nikita.sem2007@gmail.com>
2021-12-03 20:06:53 +0300

    создание проекта

---
+++ test-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <myComponents></myComponents>
+</template>
+
+<script>
+import myComponents from './components/myComponents.vue'
+
+export default {
+  name: 'App',
+  components: {
+    myComponents
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ test-vue/src/components/myComponents.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>ЗАГАЛОВОК</h1>
+<p>описание</p>
+</template>
+
+<script>
+export default {
+    name: 'myComponents',
+    data() {
+        return
+    }
+}
+</script>
+
+<style>
+</style>
